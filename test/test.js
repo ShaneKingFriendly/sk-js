93ccf979e48becf9ed23a3df0810f6fe29f91766
Shane King <github@azolla.org>
2016-03-30 13:31:08 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,11 +2,55 @@
 var simple = require('../src/sk');
 var expect = require('expect.js');
 
-describe("simple", function(){
-  it("should return true", function(){
-    expect(simple.$sk.inValid({})).to.equal(true);
+describe("1", function(){
+  describe("a", function(){
+    it("should return true", function(){
+      expect($sk.a([])).to.equal(true);
+    });
+    it("should return false", function(){
+      expect($sk.a(null)).to.equal(false);
+    });
   });
-  it("should return false", function(){
-    expect(simple.$sk.inValid(null)).to.equal(false);
+  describe("b", function(){
+    it("should return true", function(){
+      expect($sk.b(false)).to.equal(true);
+      expect($sk.b("false")).to.equal(false);
+    });
+    it("should return false", function(){
+      expect($sk.b("b")).to.equal(false);
+    });
   });
+  describe("d", function(){
+    it("should return true", function(){
+      expect($sk.d(new Date())).to.equal(true);
+    });
+    it("should return false", function(){
+      expect($sk.d("Invalid Date")).to.equal(false);
+    });
+  });
+  describe("n", function(){
+    it("should return true", function(){
+      expect($sk.n("1")).to.equal(true);
+    });
+    it("should return false", function(){
+      expect($sk.n("n")).to.equal(false);
+    });
+  });
+  describe("o", function(){
+    it("should return true", function(){
+      expect($sk.o({})).to.equal(true);
+    });
+    it("should return false", function(){
+      expect($sk.o("o")).to.equal(false);
+    });
+  });
+  describe("s", function(){
+    it("should return true", function(){
+      expect($sk.s("s")).to.equal(true);
+    });
+    it("should return false", function(){
+      expect($sk.s(undefined)).to.equal(false);
+    });
+  });
+  
 });
