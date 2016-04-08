72cdbf26af3b0ae2827ca1bedfa4d602a3441389
ShaneKing <github@azolla.org>
2016-04-08 23:02:58 +0800

    Array.isArray

--- src/sk.js
+++ src/sk.js
@@ -26,11 +26,66 @@
   };
 
   // insert all source code here
-  // sk body here
+  // copy from jQuery
+  $sk.extend = function () {
+    var options, name, src, copy, copyIsArray, clone,
+      target = arguments[0] || {},
+      i = 1,
+      length = arguments.length,
+      deep = false;
+    // Handle a deep copy situation
+    if (typeof target === "boolean") {
+      deep = target;
+      // Skip the boolean and the target
+      target = arguments[i] || {};
+      i++;
+    }
+    // Handle case when target is a string or something (possible in deep copy)
+    if (typeof target !== "object" && !jQuery.isFunction(target)) {
+      target = {};
+    }
+    // Extend jQuery itself if only one argument is passed
+    if (i === length) {
+      target = this;
+      i--;
+    }
+    for (; i < length; i++) {
+      // Only deal with non-null/undefined values
+      if (( options = arguments[i] ) != null) {
+        // Extend the base object
+        for (name in options) {
+          src = target[name];
+          copy = options[name];
+          // Prevent never-ending loop
+          if (target === copy) {
+            continue;
+          }
+          // Recurse if we're merging plain objects or arrays
+          if (deep && copy && ( jQuery.isPlainObject(copy) ||
+            ( copyIsArray = jQuery.isArray(copy) ) )) {
+            if (copyIsArray) {
+              copyIsArray = false;
+              clone = []//src && jQuery.isArray( src ) ? src : []; //sk different with jQuery
+            } else {
+              clone = src && jQuery.isPlainObject(src) ? src : {};
+            }
+            // Never move original objects, clone them
+            target[name] = extend(deep, clone, copy);
+            // Don't bring in undefined values
+          } else if (copy !== undefined) {
+            target[name] = copy;
+          }
+        }
+      }
+    }
+    // Return the modified object
+    return target;
+  };
 
+  // sk body here
   //Always return valid Array, if invalid return empty array
   $sk.a = function (array) {
-    return jQuery.isArray(array) ? array : [];
+    return Array.isArray(array) ? array : [];
   };
 
   //Just true return true, other return false
