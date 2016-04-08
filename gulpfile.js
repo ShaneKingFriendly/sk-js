cf036a7371239c37e58a312389cdd55f53b989b0
ShaneKing <github@azolla.org>
2016-04-08 20:10:38 +0800

    quote " -> '

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
 
-gulp.task("default", function() {
+gulp.task('default', function() {
   // 将你的默认的任务代码放在这
 });
--- src/sk.js
+++ src/sk.js
@@ -1,13 +1,13 @@
 (function (global, factory) {
-  if (typeof module === "object" && typeof module.exports === "object") {
+  if (typeof module === 'object' && typeof module.exports === 'object') {
     // CMD
     // all dependencies need to passed as parameters manually,
     // will not require here.
     module.exports = factory;
-  } else if (typeof define === "function" && define.amd) {
+  } else if (typeof define === 'function' && define.amd) {
     // AMD. Register as sk
     // TODO how to define the jquery plugin here?
-    define("sk", ["jsface", "jquery"], factory);
+    define('sk', ['jsface', 'jquery'], factory);
   } else {
     // in browser, global is window.
     // all dependencies were loaded already.
@@ -15,7 +15,7 @@
     // expose $sk and all components to window.
     factory(global, jsface, jQuery);
   }
-}(typeof window !== "undefined" ? window : this, function (window, jsface, jQuery, DO_NOT_EXPOSE_SK_TO_GLOBAL) {
+}(typeof window !== 'undefined' ? window : this, function (window, jsface, jQuery, DO_NOT_EXPOSE_SK_TO_GLOBAL) {
   var _sk = window.$sk;
   var $sk = {};
   window.$sk = $sk;
@@ -35,13 +35,13 @@
 
   //Just true return true, other return false
   $sk.b = function (boolean) {
-    return String(boolean) === "true" && boolean !== "true" && boolean;
+    return String(boolean) === 'true' && boolean !== 'true' && boolean;
   };
 
   //Always return valid Date, if invalid return defaultDate or new Date()
   $sk.d = function (date, defaultDate) {
     var rtnDate = defaultDate ? defaultDate : new Date();
-    return (date instanceof Date) ? (date.toString() === "Invalid Date" ? rtnDate : date) : rtnDate;
+    return (date instanceof Date) ? (date.toString() === 'Invalid Date' ? rtnDate : date) : rtnDate;
   };
 
   //Can be to Number than return value of number, other return 0
@@ -60,7 +60,7 @@
   };
 
   // reset to old $sk
-  if (typeof DO_NOT_EXPOSE_SK_TO_GLOBAL !== "undefined" && DO_NOT_EXPOSE_SK_TO_GLOBAL === true) {
+  if (typeof DO_NOT_EXPOSE_SK_TO_GLOBAL !== 'undefined' && DO_NOT_EXPOSE_SK_TO_GLOBAL === true) {
     window.$sk = _sk;
   }
   return $sk;
