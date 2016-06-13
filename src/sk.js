5e2d9844ffe24c141756b0b17669e6e8edda0703
ShaneKing <github@azolla.org>
2016-06-13 21:29:26 +0800

    rmv extends & classNames

--- src/sk.js
+++ src/sk.js
@@ -1,7 +1,7 @@
 ;(function (global, factory) {
   'use strict';
 
-  if (typeof define === 'function' && define.amd && define.amd.jQuery) {
+  if (typeof define === 'function' && define.amd) {
     //AMD
     // register as 'sk-js', consistent with npm package name
     define('sk-js', ['jquery'], function ($) {
@@ -103,128 +103,6 @@
     return target;
   };
 
-  /**
-   * [deep ], target, object1 [, objectN ]/array1 [, arrayN]
-   */
-  $sk.extends = function () {
-    var options,
-      target = arguments[0] || {},
-      i = 1,
-      length = arguments.length,
-      deep = false;
-    // Handle a deep copy situation
-    if (typeof target === "boolean") {
-      deep = target;
-      // Skip the boolean and the target
-      target = arguments[i] || {};
-      i++;
-    }
-    // Handle case when target is a string or something (possible in deep copy)
-    if (typeof target !== "object" && !jQuery.isFunction(target)) {
-      target = {};
-    }
-    // Extend jQuery itself if only one argument is passed
-    if (i === length) {
-      target = this;
-      i--;
-    }
-    for (; i < length; i++) {
-      // Only deal with non-null/undefined values
-      if ((options = arguments[i] ) != null) {
-        if (Array.isArray(options)) {
-          options.forEach(function (option) {
-            $sk.extend(deep, target, option);
-          });
-        } else {
-          $sk.extend(deep, target, options);
-        }
-      }
-    }
-    // Return the modified object
-    return target;
-  };
-
-  //copy from classNames
-  // don't inherit from Object so we can skip hasOwnProperty check later
-  // http://stackoverflow.com/questions/15518328/creating-js-object-with-object-createnull#answer-21079232
-  function StorageObject() {
-  }
-
-  StorageObject.prototype = Object.create(null);
-
-  function _parseString4ClassNames(resultSet, str) {
-    var array = str.split($sk.REGEXP_SPACE);
-
-    for (var i = 0; i < array.length; ++i) {
-      resultSet[array[i]] = true;
-    }
-  }
-
-  function _parseObject4ClassNames(resultSet, object) {
-    for (var k in object) {
-      if ($sk.OWN_PROP_OF_OBJECT.call(object, k)) {
-        // set value to false instead of deleting it to avoid changing object structure
-        // https://www.smashingmagazine.com/2012/11/writing-fast-memory-efficient-javascript/#de-referencing-misconceptions
-        resultSet[k] = !!object[k];
-      }
-    }
-  }
-
-  function _parseNumber4ClassNames(resultSet, num) {
-    resultSet[num] = true;
-  }
-
-  function _parse4ClassNames(resultSet, arg) {
-    if (!arg) return;
-    var argType = typeof arg;
-
-    // 'foo bar'
-    if (argType === $sk.TYPE_OF_STRING) {
-      _parseString4ClassNames(resultSet, arg);
-
-      // ['foo', 'bar', ...]
-    } else if (Array.isArray(arg)) {
-      _parseArray4ClassNames(resultSet, arg);
-
-      // { 'foo': true, ... }
-    } else if (argType === $sk.TYPE_OF_OBJECT) {
-      _parseObject4ClassNames(resultSet, arg);
-
-      // '130'
-    } else if (argType === $sk.TYPE_OF_NUMBER) {
-      _parseNumber4ClassNames(resultSet, arg);
-    }
-  }
-
-  function _parseArray4ClassNames(resultSet, array) {
-    for (var i = 0; i < array.length; ++i) {
-      _parse4ClassNames(resultSet, array[i]);
-    }
-  }
-
-  $sk.classNames = function () {
-    // don't leak arguments
-    // https://github.com/petkaantonov/bluebird/wiki/Optimization-killers#32-leaking-arguments
-    var len = arguments.length;
-    var args = new Array(len);
-    for (var i = 0; i < len; i++) {
-      args[i] = arguments[i];
-    }
-
-    var classSet = new StorageObject();
-    _parseArray4ClassNames(classSet, args);
-
-    var list = [];
-
-    for (var k in classSet) {
-      if (classSet[k]) {
-        list.push(k)
-      }
-    }
-
-    return list.join(' ');
-  };
-
   // sk body here
   var _context = window;
   $sk.$ = function (context, $) {
@@ -235,22 +113,20 @@
     }
     return innerContext[inner$];
   };
+  
   //Always return valid Array, if invalid return empty array
   $sk.a = function (array) {
     return Array.isArray(array) ? array : [];
   };
-
   //Just true return true, other return false
   $sk.b = function (boolean) {
     return String(boolean) === 'true' && boolean !== 'true' && boolean;
   };
-
   //Always return valid Date, if invalid return defaultDate or new Date()
   $sk.d = function (date, defaultDate) {
     var rtnDate = defaultDate ? defaultDate : new Date();
     return (date instanceof Date) ? (date.toString() === 'Invalid Date' ? rtnDate : date) : rtnDate;
   };
-
   $sk.isA = function (a) {
     return (typeof a == 'object') && a.constructor == Array;
   };
@@ -269,17 +145,14 @@
   $sk.isS = function (s) {
     return (typeof s == 'string') && s.constructor == String;
   };
-
   //Can be to Number than return value of number, other return 0
   $sk.n = function (number, defaultNumber) {
     return isNaN(Number(number)) ? (defaultNumber ? defaultNumber : 0) : Number(number);
   };
-
   //Always return valid Object, if invalid return empty object
   $sk.o = function (object) {
     return jQuery.isPlainObject(object) ? object : {};
   };
-
   //Return the String of input
   $sk.s = function (string) {
     return $sk.ARR_OF_BAD_VALUE.indexOf(String(string)) === -1 ? String(string) : '';
