a70ad6ad09f58315b06d65deb2d41819814164aa
ShaneKing <github@azolla.org>
2016-06-22 22:22:46 +0800

    format

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,21 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   return gulp.src(['dist'], {read: false})
     .pipe(clean());
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return gulp.src('src/*.js')
     .pipe(gulp.dest('dist/'))
     .pipe(sourcemaps.init())
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', ['clean'], function() {
+gulp.task('default', ['clean'], function () {
   gulp.start('scripts');
 });
