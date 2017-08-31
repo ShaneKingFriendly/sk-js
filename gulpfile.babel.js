e52bf65c8104e3691c04736ae7b07c5e7b58dbdf
ShaneKing <github@azolla.org>
2017-08-31 21:42:34 +0800

    0.1.24

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,4 @@
 import gulp from 'gulp';
-import gbabel from 'gulp-babel';
 import gclean from 'gulp-clean';
 import guglify from 'gulp-uglify';
 import grename from 'gulp-rename';
@@ -7,13 +6,12 @@ import gsourcemaps from 'gulp-sourcemaps';
 import gwebpack from 'webpack-stream';
 import distJsWebpack from './webpack.distJs.config.babel';
 
-gulp.task('default', ['distJs', 'lib'], () => {});
-
-gulp.task('distJs', ['distJsClean'], () => {
-  gulp.start('distJsBuild');
+gulp.task('clean', () => {
+  return gulp.src(['dist'], {read: false})
+    .pipe(gclean());
 });
-gulp.task('distJsBuild', () => {
-  return gulp.src('src/*.js')
+gulp.task('scripts', () => {
+  return gulp.src('src/sk.js')
     .pipe(gwebpack(distJsWebpack))
     .pipe(gulp.dest('dist/'))
     .pipe(gsourcemaps.init())
@@ -22,32 +20,6 @@ gulp.task('distJsBuild', () => {
     .pipe(gsourcemaps.write('./'))
     .pipe(gulp.dest('dist/'));
 });
-gulp.task('distJsClean', () => {
-  return gulp.src(['dist'], {read: false})
-    .pipe(gclean());
-});
-
-gulp.task('lib', ['libClean'], () => {
-  gulp.start('libBuild');
-});
-gulp.task('libBuild', () => {
-  return gulp.src('src/**/*.js')
-    .pipe(gbabel({
-      'presets': [
-        'es2015',
-        'stage-0',
-        'stage-1',
-        'stage-2',
-        'stage-3'
-      ],
-      'plugins': [
-        'add-module-exports'
-      ],
-      'sourceMaps': 'both'
-    }))
-    .pipe(gulp.dest('lib'));
-});
-gulp.task('libClean', () => {
-  return gulp.src(['lib'], {read: false})
-    .pipe(gclean());
+gulp.task('default', ['clean'], () => {
+  gulp.start('scripts');
 });
