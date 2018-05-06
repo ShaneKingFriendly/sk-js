27414af2a091847d7100da6022d824fe41b5a563
ShaneKing <github@azolla.org>
2018-05-06 20:45:17 +0800

    Date + Time => Datetime

--- src/SKL10nEntity.js
+++ src/SKL10nEntity.js
@@ -2,7 +2,7 @@ import SK from './SK';
 import SKEntity from './SKEntity';
 
 export default SK.extends(true, {}, SKEntity, {
-  createTimeZone: undefined,
-  lastModifyTimeZone: undefined,
-  invalidTimeZone: undefined,
+  createTimezone: undefined,
+  lastModifyTimezone: undefined,
+  invalidTimezone: undefined,
 })
