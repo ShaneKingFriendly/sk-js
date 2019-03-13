6d3135137521919e0d5dd520d5c5ad23cd21c265
ShaneKing <github@azolla.org>
2019-03-13 23:10:41 +0800

    0.3.12 $any

--- src/Proxy0.js
+++ src/Proxy0.js
@@ -7,7 +7,9 @@ import _ from 'lodash';
 export default class Proxy0 {
   static CryptoJS = CryptoJS;
   static $ = $;
+  static $any = any => $(any);
   static Cookies = Cookies;
   static md5 = md5;
+  static md5Any = any => md5(any);
   static _ = _;
 }
