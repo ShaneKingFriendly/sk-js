6185101c31a2489da9f99e8c34ab785cc7331617
ShaneKing <github@azolla.org>
2019-07-09 00:55:39 +0800

    adjust SKEntity

--- src/SKEntity.js
+++ src/SKEntity.js
@@ -1,13 +1,4 @@
 export default {
-  createDatetime: undefined,
-  createUserId: undefined,
-  extJson: undefined,
-  extJsonStr: undefined,
-  id: undefined,
-  invalid: undefined,
-  invalidDatetime: undefined,
-  invalidUserId: undefined,
-  lastModifyDatetime: undefined,
-  lastModifyUserId: undefined,
-  version: undefined,
+  pageHelper: undefined,
+  whereOCs: undefined,
 }
