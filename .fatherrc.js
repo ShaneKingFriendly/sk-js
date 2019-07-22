4b5058f75ea460e387e957ac560c5b6ea2026205
ShaneKing <github@azolla.org>
2019-07-22 23:41:39 +0800

    0.5.20 father

---
+++ .fatherrc.js
@@ -0,0 +1,9 @@
+export default {
+  // cjs: { type: 'babel' },
+  cjs: 'babel',
+  doc: {
+    base: '/sk-js',
+  },
+  entry: 'src/index.js',
+  esm: 'babel',
+};
--- doczrc.js
+++
@@ -1,6 +0,0 @@
-export default {
-  base: '/sk-js/',
-  themeConfig: {
-    mode: 'light',
-  },
-};
